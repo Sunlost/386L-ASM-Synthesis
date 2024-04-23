524f2c2b1ce0c642c0fea0f954da392b8146a47d
Sun <suncs@utexas.edu>
2024-04-23 13:31:29 -0500

    risc tests

--- cross-synthesis.rkt
+++ cross-synthesis.rkt
@@ -700,9 +700,6 @@
 
 (module+ test
 
-
-
-
 ; test lc3b implementation
 
 ; define our starting state for tests run below
@@ -760,9 +757,6 @@
 ; - instruction memory
 ;     [nothing]
 
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
 
 ; define state for test one
 (define test-one-state 
@@ -806,9 +800,6 @@
     (check-equal? (final-state (addr 1056)) (val 0))
 )
 
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
 
 ; define state for test two
 (define test-two-state 
@@ -855,11 +846,8 @@
     (check-equal? (final-state (addr 1056)) (val 0))
 )
 
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
-; ----------------------------------------------------------------------------------------------- ;
 
-; define state for test two
+; define state for test three
 (define test-three-state 
   (set-state (set-state (set-state (set-state (set-state (set-state (set-state (set-state
   (set-state
@@ -888,6 +876,61 @@
     (check-equal?          (final-state x0) (val 4))      ; altered from starting state
     (check-equal?          (final-state x7) (addr 2016))  ; altered from starting state
     (check-equal?  (final-state COND_CODES) 3_P_True)   ; altered from starting state
+)
+
+
+; test risc-v implementation
+
+
+;; basically same as lc3b test three
+(define risc-test-one-state
+  (set-state (set-state (set-state (set-state (set-state (set-state (set-state (set-state
+
+  example-state
+
+  (addr 2000) (5_XOR x0 x0 x0))
+  (addr 2004) (5_ADDI x0 (imm5 1) x0))
+  (addr 2008) (5_ADDI x0 (imm5 1) x0))
+  (addr 2012) (5_JAL (addr 2024) x7))    
+  (addr 2016) (5_ADDI x0 (imm5 1) x0))
+  (addr 2020) HLT)
+
+  (addr 2024) (5_ADDI x0 (imm5 1) x0))
+  (addr 2028) (5_JALR x7 (imm12 0) x1)) ; return
+)
+
+(let ([final-state (eval-riscv-prog risc-test-one-state)])
+    (check-equal?          (final-state x0) (val 4))      ; altered from starting state
+    (check-equal?          (final-state x1) (addr 2032))  ; altered from starting state
+    (check-equal?          (final-state x7) (addr 2016))  ; altered from starting state
+)
+
+
+
+;; basically same as lc3b test three
+(define risc-test-two-state
+  (set-state (set-state (set-state (set-state (set-state (set-state (set-state (set-state
+  (set-state
+
+  example-state
+
+  (addr 2000) (5_XOR x0 x0 x0))
+  (addr 2004) (5_XOR x1 x1 x1))
+  (addr 2008) (5_ADDI x0 (imm5 12) x0))
+  (addr 2012) (5_ADDI x1 (imm5 15) x1))
+  ; L1.
+  (addr 2016) (5_BGE x1 x0 (imm12 12))) ; BGEU x1 x0 .L2
+  (addr 2020) (5_ADDI x0 (imm5 1) x0))
+  (addr 2024) HLT)
+  ; L2.
+  (addr 2028) (5_AND x0 x1 x1))
+  (addr 2032) (5_BEQ x0 x1 (imm12 -12)))  ; BEQ  x0 x1 .L1
+)
+
+(let ([final-state (eval-riscv-prog risc-test-two-state)])
+    (check-equal?          (final-state x0) (val 13))      ; altered from starting state
+    (check-equal?          (final-state x1) (val 12))      ; altered from starting state
+    (check-equal?          (final-state PC) (addr 2024))   ; altered from starting state
 )
 
 ) ; /module+ test
